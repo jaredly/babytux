dff260daf7949a3bb9fcc2d02a47e0ff9d52fdd0
Jared Forsyth <jared@jaredforsyth.com>
2010-04-26 20:19:01 -0400

    making projection better, understanding sprites

--- camera.py
+++ camera.py
@@ -5,7 +5,7 @@ from pyglet.gl import *
 
 class Camera(object):
 
-    def __init__(self, win, x=0.0, y=0.0, rot=0.0, zoom=1.0):
+    def __init__(self, win, x=0.0, y=0.0, rot=0.0, zoom=100.0):
         self.win = win
         self.x = x
         self.y = y
@@ -16,11 +16,24 @@ class Camera(object):
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
         widthRatio = self.win.width / self.win.height
-        gluOrtho2D(
+        look = 0, self.win.width, 0, self.win.height
+        look = self.scale(look, 5)
+        gluOrtho2D(*look)
+        glMatrixMode(GL_MODELVIEW)
+        '''
             -self.zoom * widthRatio,
             self.zoom * widthRatio,
             -self.zoom,
             self.zoom)
+        '''
+
+    def scale(self, look, scale):
+        w = look[1]-look[0]
+        h = look[3]-look[2]
+        m = (look[1]+look[0])/2.0, (look[3]+look[2])/2.0
+        return m[0] - w*scale/2, m[0] + w*scale/2, m[1] - h*scale/2, m[1] + h*scale/2
+
+
 
     def hudProjection(self):
         glMatrixMode(GL_PROJECTION)
